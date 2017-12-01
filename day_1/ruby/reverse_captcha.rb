54dfd23bda036a64a4c3462ec9cdf3d434c4d8c0
Rhys Stansfield <rhys.m.stansfield@gmail.com>
2017-12-01 19:17:22 +0000

    Refactor ruby day_1

--- day_1/ruby/reverse_captcha.rb
+++ day_1/ruby/reverse_captcha.rb
@@ -1,40 +1,11 @@
 # no-doc
-class ReverseCaptcha
-  def self.calculate(input)
-    new(input).calculate
-  end
-
-  def initialize(input)
-    self.chars = input.chars << input[0]
-  end
-
-  def calculate
-    values.inject(0, :+)
-  end
-
-  private
-
-  def values
-    chars.map.with_index do |char, i|
-      int = char.to_i
-
-      int == chars[i + 1].to_i ? int : 0
-    end
-  end
-
-  attr_accessor :chars
-end
-
-# no-doc
-class ReverseCaptchaModulo
+class ReverseCaptchaBase
   def self.calculate(input)
     new(input).calculate
   end
 
   def initialize(input)
     self.chars = input.chars
-    self.limit = chars.length
-    self.step_distance = chars.length / 2
   end
 
   def calculate
@@ -47,9 +18,43 @@ class ReverseCaptchaModulo
     chars.map.with_index do |char, i|
       int = char.to_i
 
-      int == chars[(i + step_distance) % limit].to_i ? int : 0
+      int == chars[next_index(i)].to_i ? int : 0
     end
   end
 
-  attr_accessor :chars, :limit, :step_distance
+  def next_index(_)
+    raise NotImplementedError
+  end
+
+  attr_accessor :chars
+end
+
+# no-doc
+class ReverseCaptcha < ReverseCaptchaBase
+  def initialize(input)
+    self.chars = input.chars << input[0]
+  end
+
+  private
+
+  def next_index(i)
+    i + 1
+  end
+end
+
+# no-doc
+class ReverseCaptchaModulo < ReverseCaptchaBase
+  def initialize(input)
+    super
+    self.limit = chars.length
+    self.step_distance = chars.length / 2
+  end
+
+  private
+
+  def next_index(i)
+    (i + step_distance) % limit
+  end
+
+  attr_accessor :limit, :step_distance
 end
