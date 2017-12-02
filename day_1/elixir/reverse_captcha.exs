25115a7a97c0249d312c6fd2d40685d9bce17687
Rhys Stansfield <rhys.m.stansfield@gmail.com>
2017-12-02 11:55:55 +0000

    Initial solution for reverse captcha modulo with elixir

--- day_1/elixir/reverse_captcha.exs
+++ day_1/elixir/reverse_captcha.exs
@@ -10,3 +10,39 @@ defmodule ReverseCaptcha do
   defp sum([_ | tail], accum), do: sum(tail, accum)
   defp sum([], accum), do: accum
 end
+
+defmodule ReverseCaptchaModulo do
+  def calculate(input) when is_binary(input) do
+    String.codepoints(input)
+    |> Enum.map(&String.to_integer/1)
+    |> generate_map()
+    |> calculate()
+  end
+
+  def calculate(%{ index_map: index_map, limit: limit, step_distance: step_distance }) do
+    Enum.reduce(index_map, 0, fn({index, int}, accum) ->
+      next_index = rem(index + step_distance, limit)
+      next_int = Map.fetch!(index_map, next_index)
+
+      accum + result(int, next_int)
+    end)
+  end
+
+  defp generate_map(list) when is_list(list), do: generate_map(generate_index_map(list))
+  defp generate_map(map) when is_map(map) do
+    %{
+      index_map: map,
+      limit: Map.size(map),
+      step_distance: div(Map.size(map), 2)
+    }
+  end
+
+  defp generate_index_map(list) do
+    [Enum.to_list(0..length(list) - 1), list]
+    |> List.zip()
+    |> Enum.into(%{})
+  end
+
+  defp result(value, _ = value), do: value
+  defp result(_, _), do: 0
+end
--- day_1/elixir/reverse_captcha_test.exs
+++ day_1/elixir/reverse_captcha_test.exs
@@ -21,7 +21,7 @@ defmodule ReverseCaptchaTest do
     end
 
     test "sums 91212129 as 9" do
-      assert ReverseCaptcha.calculate("9") == 9
+      assert ReverseCaptcha.calculate("91212129") == 9
     end
 
     test "sums test input as 1089" do
@@ -31,6 +31,41 @@ defmodule ReverseCaptchaTest do
               |> String.trim()
 
       assert ReverseCaptcha.calculate(input) == 1089
+    end
+  end
+end
+
+defmodule ReverseCaptchaModuloTest do
+  use ExUnit.Case, async: true
+
+  describe "ReverseCaptchaModulo.calculate/1" do
+    test "sums 1212 as 6" do
+      assert ReverseCaptchaModulo.calculate("1212") == 6
+    end
+
+    test "sums 1221 as 0" do
+      assert ReverseCaptchaModulo.calculate("1221") == 0
+    end
+
+    test "sums 123425 as 4" do
+      assert ReverseCaptchaModulo.calculate("123425") == 4
+    end
+
+    test "sums 123123 as 12" do
+      assert ReverseCaptchaModulo.calculate("123123") == 12
+    end
+
+    test "sums 12131415 as 4" do
+      assert ReverseCaptchaModulo.calculate("12131415") == 4
+    end
+
+    test "sums test input as 1089" do
+      input = Path.expand(".")
+              |> Path.join("input.txt")
+              |> File.read!()
+              |> String.trim()
+
+      assert ReverseCaptchaModulo.calculate(input) == 1156
     end
   end
 end
